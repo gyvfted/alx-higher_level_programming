abdbd2d1b2a61853f6a254ad62566f5facf11934
Gift Obia <gyvfted3@gmail.com>
2023-10-30 13:47:40 -0700

    /**  * check_cycle - Checks if a singly linked list has a cycle.  * @list: Pointer to list.  * Return: 0 if no cycle, 1 cycle.

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,43 +1,22 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ * check_cycle - Checks if a singly linked list has a cycle.
+ * @list: Pointer to list.
+ * Return: 0 if no cycle, 1 cycle.
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *fast;
-	listint_t *slow;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	fast = list;
-	slow = list;
-	while (list && fast && fast->next)
+	while (fast != NULL && slow != NULL && fast->next != NULL)
 	{
-		list = list->next;
+		slow = slow->next;
 		fast = fast->next->next;
-
-		if (list == fast)
-		{
-			list = slow;
-			slow =  fast;
-			while (1)
-			{
-				fast = slow;
-				while (fast->next != list && fast->next != slow)
-				{
-					fast = fast->next;
-				}
-				if (fast->next == list)
-					break;
-
-				list = list->next;
-			}
+		if (slow == fast)
 			return (1);
-		}
 	}
-
 	return (0);
 }
