2f57a61c14bdd85582a2ec957d6d14b78df48176
Gift Obia <gyvfted3@gmail.com>
2023-11-02 15:14:06 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python3
+#!/bin/usr/bin/ptyhon3
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
+
     from magic_calculation_102 import add, sub
-
-    if a < b:
+    if (a < b):
         c = add(a, b)
-        for x in range(4, 6):
-            c = add(c, x)
-        return (c)
-
-    else:
-        return(sub(a, b))
+        for z in range(4, 6):
+            c = add(c, z)
+        return(c)
+    return(sub(a, b))
