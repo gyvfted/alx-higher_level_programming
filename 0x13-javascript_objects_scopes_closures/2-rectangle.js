f5057eefdd6a05bac17ccceefd0ffa2ecfb46ddd
Gift Obia <gyvfted3@gmail.com>
2024-02-13 19:01:00 +0000

    changed

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
-};
+}
+
+module.exports = Rectangle;
