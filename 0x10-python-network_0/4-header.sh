b168300d0f5dc52319d4636f3b7b463a2b515e36
Gift Obia <gyvfted3@gmail.com>
2024-02-29 16:29:11 -0800

    header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#pass a variable in the header of the request..
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
